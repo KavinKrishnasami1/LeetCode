160ee1e11d4ff2cad4d564f91fc11847c5611122
KavinKrishnasami1 <kavinsk2@illinois.edu>
2021-08-29 21:14:29 -0700

    Time: 206 ms (10.55%), Space: 21.7 MB (17.94%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,6 +1,10 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0 or grid == None:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if grid == None or len(grid) == 0:
             return 0
         
         def dfs(i, j):
@@ -18,12 +22,10 @@ class Solution:
             dfs(i, j - 1)
             
             return 1
-            
-        numIslands = 0
+        
+        num = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == '1':
-                    numIslands += dfs(i, j)
-                    
-        return numIslands
-    
+                    num += dfs(i, j)
+        return num
