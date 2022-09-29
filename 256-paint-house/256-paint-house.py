1bbb61b8ed3675cfa5311879b20753b0d4d128b5
KavinKrishnasami1 <kavinsk2@illinois.edu>
2022-09-29 02:24:04 -0500

    Time: 111 ms (46.99%), Space: 13.8 MB (88.11%) - LeetHub

--- 256-paint-house/256-paint-house.py
+++ 256-paint-house/256-paint-house.py
@@ -1,22 +1,10 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        self.dict = {}
-        if not costs:
-            return 0
+        for i in range(len(costs) - 2, -1, -1):
+            costs[i][0] += min(costs[i + 1][1], costs[i + 1][2])
+            costs[i][1] += min(costs[i + 1][0], costs[i + 1][2])
+            costs[i][2] += min(costs[i + 1][1], costs[i + 1][0])
         
-        def helper(n, color):
-            if (n, color) in self.dict:
-                return self.dict[(n, color)]
-            
-            cost = costs[n][color]
-            if n != len(costs) - 1:
-                if color == 0:
-                    cost += min(helper(n + 1, 1), helper(n + 1, 2))
-                elif color == 1:
-                    cost += min(helper(n + 1, 0), helper(n + 1, 2))
-                elif color == 2:
-                    cost += min(helper(n + 1, 0), helper(n + 1, 1))
-            self.dict[(n, color)] = cost
-            return cost
         
-        return min(helper(0, 0), helper(0, 1), helper(0, 2))
+        return min(costs[0])
+    
