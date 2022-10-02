39eb07f8294dcaf6c5e522e3dc7baa1364b15a46
KavinKrishnasami1 <kavinsk2@illinois.edu>
2022-10-02 17:43:17 -0500

    Time: 98 ms (86.46%), Space: 15.5 MB (79.40%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,23 +1,12 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        offsets = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
-        def bfs(x, y):
-            visited = set()
-            queue = deque([(0, 0)])
-            steps = 0
+        @lru_cache(maxsize = None)
+        def dfs(x, y):
+            if x + y == 0:
+                return 0
+            elif x + y == 2:
+                return 2
+            else:
+                return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
             
-            while queue:
-                count = len(queue)
-                for i in range(count):
-                    currX, currY = queue.popleft()
-                    if (currX, currY) == (x, y):
-                        return steps
-                    for offsetX, offsetY in offsets:
-                        nextX, nextY = currX + offsetX, currY + offsetY
-                        if (nextX, nextY) not in visited:
-                            visited.add((nextX, nextY))
-                            queue.append((nextX, nextY))
-                steps += 1
-            
-        
-        return bfs(x, y)
+        return dfs(abs(x), abs(y))
