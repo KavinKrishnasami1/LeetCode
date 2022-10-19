895bf9746ce2fadd563c7187970ba2d87507dfec
KavinKrishnasami1 <kavinsk2@illinois.edu>
2022-10-19 17:43:24 -0500

    Time: 1131 ms (77.55%), Space: 76.6 MB (23.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,21 +1,53 @@
 from collections import OrderedDict
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dict = OrderedDict()
+        self.dict = dict()
         self.capacity = capacity
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
         if key in self.dict:
-            self.dict.move_to_end(key)
-            return self.dict[key]
+            n = self.dict[key]
+            self.remove(n)
+            self.add(n)
+            return n.val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        self.dict[key] = value
-        self.dict.move_to_end(key)
+        if key in self.dict:
+            self.remove(self.dict[key])
+        n = Node(key, value)
+        self.add(n)
+        self.dict[key] = n
         if len(self.dict) > self.capacity:
-            self.dict.popitem(last = False)
+            n = self.head.next
+            self.remove(n)
+            del self.dict[n.key]
+        
+    def remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+    
+    def add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
             
 
 
